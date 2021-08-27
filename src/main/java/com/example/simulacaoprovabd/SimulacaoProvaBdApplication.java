1c6207cbafadb2271439c20cd8e6bc635510afcd
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2021-08-27 15:37:12 -0300

    Spring startr project

---
+++ src/main/java/com/example/simulacaoprovabd/SimulacaoProvaBdApplication.java
@@ -0,0 +1,13 @@
+package com.example.simulacaoprovabd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacaoProvaBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulacaoProvaBdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simulacaoprovabd/SimulacaoProvaBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simulacaoprovabd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacaoProvaBdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
